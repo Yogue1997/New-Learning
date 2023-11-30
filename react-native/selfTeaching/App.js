d3336064f8c0b6330fd770f341e4dce2edf503a4
Yogue1997 <yousyogue@gmail.com>
2023-11-30 01:27:40 -0500

    specific size of an image

--- react-native/selfTeaching/App.js
+++ react-native/selfTeaching/App.js
@@ -6,7 +6,11 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>Hello Youssouf !</Text>
-      <Image source={require('./assets/favicon.png')}/>
+      <Image source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"
+      }}/>
     </SafeAreaView>
   );
 }
