8f70d7b6a076e2117bf98975019506546f0de4b2
Yogue1997 <yousyogue@gmail.com>
2023-11-30 13:42:08 -0500

    flexwrap

--- react-native/selfTeaching/App.js
+++ react-native/selfTeaching/App.js
@@ -13,23 +13,34 @@ export default function App() {
       flex: 1,
       flexDirection: "row",
       justifyContent: "center",
-      alignItems: "flex-end"
+      alignItems: "center",
+      alignContent: "center",
+      flexWrap: "wrap",
     }}>
       <View style={{
         backgroundColor: "dodgerblue",
         width: 100,
-        height: 100
+        height: 300
       }}/>
       <View style={{
         backgroundColor: "gold",
         width: 100,
-        height: 200,
-        alignSelf: "flex-start"
+        height: 100,
       }}/>
       <View style={{
         backgroundColor: "tomato",
         width: 100,
-        height: 300
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: "grey",
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: "green",
+        width: 100,
+        height: 100
       }}/>
     </View>
   );
