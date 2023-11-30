cb3db69c368c701644c4c3773f260641b68b0edf
Yogue1997 <yousyogue@gmail.com>
2023-11-30 10:50:39 -0500

    Alert.alert Very import wehn the alert comes out for teh user to choosen yes or no

--- react-native/selfTeaching/App.js
+++ react-native/selfTeaching/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, Button} from 'react-native';
+import { StyleSheet, SafeAreaView, Alert, Button} from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text cliked");
@@ -8,7 +8,10 @@ export default function App() {
       <Button
         color="orange"
         title='click me'
-        onPress={() => alert('Button Tape')}
+        onPress={() => Alert.alert("My title", "My message", [
+          {text: "Yes", onPress: () => console.log("Yes")},
+          {text: "No", onPress: () => console.log("No")}
+        ])}
       />
     </SafeAreaView>
   );
