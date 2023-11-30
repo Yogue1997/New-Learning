a16a35702e0b6b9315825215790cfd1c956b8c12
Yogue1997 <yousyogue@gmail.com>
2023-11-30 01:14:39 -0500

    onPress adn numberOfLines

--- react-native/selfTeaching/App.js
+++ react-native/selfTeaching/App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text cliked");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello Youssouf!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>Hello Youssouf !</Text>
     </SafeAreaView>
   );
 }
@@ -13,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
