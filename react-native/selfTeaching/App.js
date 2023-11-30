36d40c240f39d323852b7fee6bcb805dfc475fc7
Yogue1997 <yousyogue@gmail.com>
2023-11-30 10:40:54 -0500

    Button

--- react-native/selfTeaching/App.js
+++ react-native/selfTeaching/App.js
@@ -1,15 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableWithoutFeedback, TouchableNativeFeedback, SafeAreaView} from 'react-native';
+import { StyleSheet, SafeAreaView, Button} from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text cliked");
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello Youssouf !</Text>
-      {/* only android */}
-      <TouchableNativeFeedback onPress={() => console.log("Image tapped")}> 
-        <View style={{ width:200, height:70, backgroundColor: "dodgerblue" }}></View>
-      </TouchableNativeFeedback>
+      <Button
+        color="orange"
+        title='click me' 
+      />
     </SafeAreaView>
   );
 }
