dbebfb78c4d69d9977eb131b8ccf556e9a53e084
Yogue1997 <yousyogue@gmail.com>
2023-12-01 12:59:41 -0500

    Finish first tutorial

--- react-native/selfTeaching/App.js
+++ react-native/selfTeaching/App.js
@@ -1,49 +1,14 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import WelcomScreen from './app/screen/WelcomScreen';
 
-import { useImageDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
-  console.log(useDeviceOrientation());
-
-  const { landscape } = useDeviceOrientation();
 
   return (
-    <View style={{
-      backgroundColor: "#fff",
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      alignContent: "center",
-    }}>
-      <View style={{
-        backgroundColor: "dodgerblue",
-        width: 100,
-        flexShrink: 1,//same as flex:-1. To help content to be in the middle instead of overflowing 
-        height: 100
-      }}/>
-      <View style={{
-        backgroundColor: "gold",
-        width: 100,
-        height: 100,
-        top: 20,
-        // bottom: 20,
-        left: 20,
-        position: "relative"
-      }}/>
-      <View style={{
-        backgroundColor: "tomato",
-        width: 100,
-        height: 100
-      }}/>
-    </View>
+    <WelcomScreen />
   );
 }
 
 // const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-//   },
+  
 // });
