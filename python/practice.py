952882292cb5bc9bf26641b448c700f6ab135219
Yogue1997 <yousyogue@gmail.com>
2023-12-02 18:40:48 -0500

    try catch

--- python/practice.py
+++ python/practice.py
@@ -1,2 +1,15 @@
 print("hello world")
 #to run it (python3 practice.py)
+
+x = 1
+if (x == 1):
+    print("x is 1")
+    print(x)
+
+#To define a floating point number, you may use one of the following notations:
+print("###float###")
+
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
